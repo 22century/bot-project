068c88c4df81c5dcd6fde7b88f86ab4fa75a02cf
22century <tnkussr+github@gmail.com>
2014-09-01 23:40:39 +0900

    db

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+class DB
+{
+    const HOST    = 'localhost';
+    const USER    = '任意のDBユーザー名';
+    const PASS    = '任意のDBパスワード';
+    const CHARSET = 'utf8';
+}
---
+++ config/db.production.php
@@ -0,0 +1,9 @@
+<?php
+
+class DB
+{
+    const HOST    = '任意のDBホスト名';
+    const USER    = '任意のDBユーザー名';
+    const PASS    = '任意のDBパスワード';
+    const CHARSET = 'utf8';
+}
